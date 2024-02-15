790ec7280282bd6365e3b4183d337dde8ba550f2
Matt <solomatias321@gmail.com>
2024-02-15 18:19:29 +0100

    create nav link for all routes

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,7 +1,34 @@
+import { bottombarLinks } from "@/constants";
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <section className="botton-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={link.label}
+            className={`leftsidebar-link group ${isActive && "bg-primary-500"}`}
+          >
+            <NavLink to={link.route} className="flex gap-4 items-center p-4">
+              <img
+                src={link.imgURL}
+                alt={link.label}
+                className={`group-hover:invert-white ${
+                  isActive && "invert-white"
+                } `}
+              />
+              {link.label}
+            </NavLink>
+          </li>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
