342507eada1e096c321ec62294c8855a24c3fa55
Matt <solomatias321@gmail.com>
2024-03-06 18:00:23 +0100

    creating liked post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from "../appwrite/api";
+import { createPost, createUserAccount, getRecentPosts, likePost, signInAccount, signOutAccount } from "../appwrite/api";
 import { INewPost, INewUser } from "@/types";
 import { QUERY_KEYS } from "./queryKeys";
 
@@ -49,5 +49,19 @@ export const useGetRecentPosts = () =>{
     return useQuery({
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
         queryFn: getRecentPosts,
+    })
+}
+
+export const useLikePost = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({ postId, likesArray }: { postId: string; likesArray:
+             string[]}) => likePost(postId, likesArray),
+             onSuccess: (data) => {
+                queryClient.invalidateQueries({
+                    queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id]
+                })
+            }
     })
 }
